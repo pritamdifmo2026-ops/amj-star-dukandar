f5589507ff7b5b51f5f4d8d7ebd4b5149de9ddc9
anushk2026a <anushk2026a@gmail.com>
2026-04-30 13:05:01 +0530

    re

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  
+
   return {
     plugins: [react()],
     resolve: {
       // Vite 6+ supports tsconfig paths resolution natively
-      // @ts-ignore - Some TS versions might not have this in their types yet
+
       tsconfigPaths: true,
     },
     server: {
